9236df200b1a2143daed9c9199936e38a76a5e8f
Beining Ouyang <63318049+BeiningOuyang@users.noreply.github.com>
2021-07-17 20:17:52 -0400

    updates2

--- frontend/src/components/PlasmidDescription.vue
+++ frontend/src/components/PlasmidDescription.vue
@@ -4,16 +4,7 @@
     Plasmid name: Cloning vector pCI-neo
 
 Source/Vendor: Promega
-https://www.promega.jp/-/media/files/resources/protocols/technical-bulletins/0/pci-neo-mammalian-expression-vector-protocol.pdf?la=en
-Plasmid Type: Mammalian expression vector
-Cloning Method:  Restriction Enzyme
-Size: 5472
-Bacterial Resistance:	 Ampicillin
-Selectable Marker:	Neomycin
-GenBank: U47120.2
-https://www.ncbi.nlm.nih.gov/nuccore/U47120.2
-More info:
-https://www.addgene.org/vector-database/2184/
+
 
 
 
